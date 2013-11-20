e11dac51f4ea98abea8033055ddab86469c71305
qitta <christop@nullcat.de>
2013-11-20 19:59:02 +0100

    providerdata is obsolate.

--- hugin/providerdata.py
+++ hugin/providerdata.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
-""" Encapsulation for provider <-> url <-> request. """
+"""
+    OBSOLATE!
+    Encapsulation for provider <-> url <-> request.
+"""
 
 from hugin.query import Query, QUERY_ATTRS
 from collections import UserDict
@@ -58,101 +61,101 @@ class ProviderData(UserDict):
             self['is_done'] = True
 
 
-if __name__ == '__main__':
-    import unittest
-
-    URL = 'http://github.com'
-
-    # dummy provider to simulate provider response
-    class Provider:
-        def __init__(self, name):
-            self.name = name
-            self._value = 'success'
-
-        def parse_response(self, _dummy_response, _dummy_search_params):
-            _dummy_response, _dummy_search_params = None, None
-            result = {
-                'critical': (None, True),
-                'failed': (None, False),
-                'success': (['url'], False),
-                'success_nothing_found': ([], True)
-            }.get(self._value)
-            print(self._value, result)
-            return result
-
-        def build_url(self, _dummy_search_params):
-            _ = _dummy_search_params
-            len(_)
-            result = {
-                'failed': None,
-                'success': URL
-            }.get(self._value)
-            print(self._value, result)
-            return result
-
-        def set_state(self, state):
-            self._value = state
-
-        def retries_left(self):
-            return self['retries_left'] > 0
-
-        def decrement_retries(self):
-            self['retries_left'] -= 1
-
-    def create_query(**kwargs):
-        return Query(QUERY_ATTRS, kwargs)
-
-    def create_dummy_provider(name):
-        return Provider(name)
-
-    class TestProviderData(unittest.TestCase):
-        def setUp(self):
-            self._provider = create_dummy_provider('hans')
-            self._query = create_query(title='Sin City', type='movie')
-            self._provider_data = ProviderData(
-                provider=self._provider,
-                query=self._query
-            )
-
-        def test_providerdata_init(self):
-            self.assertTrue(self._provider_data.query is self._query)
-            self.assertFalse(self._provider_data.is_done)
-
-        def test_provider_build_url(self):
-            self._provider.set_state('success')
-            self._provider_data.invoke_build_url()
-            self.assertFalse(self._provider_data.is_done)
-            self.assertTrue(self._provider_data['url'] == URL)
-
-            self._provider.set_state('failed')
-            self._provider_data.invoke_build_url()
-            self.assertTrue(self._provider_data.is_done)
-            self.assertTrue(self._provider_data['url'] is None)
-
-        def test_provider_parse_response(self):
-            self._provider.set_state('success')
-            self._provider_data.invoke_parse_response()
-
-            self.assertFalse(self._provider_data.is_done)
-            self.assertTrue(self._provider_data.has_valid_result)
-
-            self._provider.set_state('success_nothing_found')
-            self._provider_data.invoke_parse_response()
-
-            self.assertTrue(self._provider_data.is_done)
-            self.assertTrue(self._provider_data.has_valid_result)
-
-            self._provider.set_state('failed')
-            for _ in range(4):
-                self._provider_data.invoke_parse_response()
-                self.assertFalse(self._provider_data.is_done)
-                self.assertFalse(self._provider_data.has_valid_result)
-            self._provider_data.invoke_parse_response()
-
-            self._provider.set_state('critical')
-            self._provider_data.invoke_parse_response()
-
-            self.assertTrue(self._provider_data.is_done)
-            self.assertFalse(self._provider_data.has_valid_result)
-
-    unittest.main()
+#if __name__ == '__main__':
+#    import unittest
+#
+#    URL = 'http://github.com'
+#
+#    # dummy provider to simulate provider response
+#    class Provider:
+#        def __init__(self, name):
+#            self.name = name
+#            self._value = 'success'
+#
+#        def parse_response(self, _dummy_response, _dummy_search_params):
+#            _dummy_response, _dummy_search_params = None, None
+#            result = {
+#                'critical': (None, True),
+#                'failed': (None, False),
+#                'success': (['url'], False),
+#                'success_nothing_found': ([], True)
+#            }.get(self._value)
+#            print(self._value, result)
+#            return result
+#
+#        def build_url(self, _dummy_search_params):
+#            _ = _dummy_search_params
+#            len(_)
+#            result = {
+#                'failed': None,
+#                'success': URL
+#            }.get(self._value)
+#            print(self._value, result)
+#            return result
+#
+#        def set_state(self, state):
+#            self._value = state
+#
+#        def retries_left(self):
+#            return self['retries_left'] > 0
+#
+#        def decrement_retries(self):
+#            self['retries_left'] -= 1
+#
+#    def create_query(**kwargs):
+#        return Query(QUERY_ATTRS, kwargs)
+#
+#    def create_dummy_provider(name):
+#        return Provider(name)
+#
+#    class TestProviderData(unittest.TestCase):
+#        def setUp(self):
+#            self._provider = create_dummy_provider('hans')
+#            self._query = create_query(title='Sin City', type='movie')
+#            self._provider_data = ProviderData(
+#                provider=self._provider,
+#                query=self._query
+#            )
+#
+#        def test_providerdata_init(self):
+#            self.assertTrue(self._provider_data.query is self._query)
+#            self.assertFalse(self._provider_data.is_done)
+#
+#        def test_provider_build_url(self):
+#            self._provider.set_state('success')
+#            self._provider_data.invoke_build_url()
+#            self.assertFalse(self._provider_data.is_done)
+#            self.assertTrue(self._provider_data['url'] == URL)
+#
+#            self._provider.set_state('failed')
+#            self._provider_data.invoke_build_url()
+#            self.assertTrue(self._provider_data.is_done)
+#            self.assertTrue(self._provider_data['url'] is None)
+#
+#        def test_provider_parse_response(self):
+#            self._provider.set_state('success')
+#            self._provider_data.invoke_parse_response()
+#
+#            self.assertFalse(self._provider_data.is_done)
+#            self.assertTrue(self._provider_data.has_valid_result)
+#
+#            self._provider.set_state('success_nothing_found')
+#            self._provider_data.invoke_parse_response()
+#
+#            self.assertTrue(self._provider_data.is_done)
+#            self.assertTrue(self._provider_data.has_valid_result)
+#
+#            self._provider.set_state('failed')
+#            for _ in range(4):
+#                self._provider_data.invoke_parse_response()
+#                self.assertFalse(self._provider_data.is_done)
+#                self.assertFalse(self._provider_data.has_valid_result)
+#            self._provider_data.invoke_parse_response()
+#
+#            self._provider.set_state('critical')
+#            self._provider_data.invoke_parse_response()
+#
+#            self.assertTrue(self._provider_data.is_done)
+#            self.assertFalse(self._provider_data.has_valid_result)
+#
+#    unittest.main()
