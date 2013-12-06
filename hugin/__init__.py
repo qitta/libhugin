664d2ed9edfbd606cd4a0aa1025ba4259463f967
kitteh <christoph@nullcat.de>
2013-12-06 10:02:39 +0100

    Session cleanup + docstrings.

--- hugin/__init__.py
+++ hugin/__init__.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
-""" Session and query handling. """
+""" Session handling for hugin core module. """
 
+# stdlib
 from concurrent.futures import ThreadPoolExecutor
 from collections import defaultdict
 from itertools import zip_longest
@@ -13,6 +14,7 @@ import sys
 import queue
 import copy
 
+# hugin
 from hugin.core.pluginhandler import PluginHandler
 from hugin.core.downloadqueue import DownloadQueue
 from hugin.core.provider.result import Result
@@ -22,10 +24,30 @@ from hugin.common.utils.stringcompare import string_similarity_ratio
 
 
 class Session:
+    """
+    Create a hugin session object, the entry point to use libhugin core.
+
+    .. note::
+
+        public methods:
+
+            create_query()
+            submit(query)
+            submit_async(query)
+
+    """
     def __init__(
         self, cache_path='/tmp', parallel_jobs=1,
         timeout_sec=5, user_agent='libhugin/1.0'
     ):
+        """
+        Init a session object with user specified parameters.
+
+        :param cache_path: Path of cache to be written to.
+        :param parallel_jobs: Number of simultanous jobs to be used.
+        :param timeout_sec: Timeout for http requests to be used.
+
+        """
         self._config = {
             'cache_path': cache_path,
             # limit parallel jobs to 4, there is no reason for a huge number of
@@ -75,14 +97,20 @@ class Session:
             self._categorize(provider)
 
     def create_query(self, **kwargs):
+        """ Return a query object build of user given kwargs. """
         return Query(kwargs)
 
     def _init_download_queue(self, query):
-        if query['use_cache'] is False:
-            query['use_cache'] = None
-        else:
-            print('cache enabled.')
+        """ Return a downloadqueue configured with user specified parameters.
+
+        :retrun: A configured downloadqueue.
+
+        """
+        if query['use_cache']:
+            print('enabling cache.')
             query['use_cache'] = self._cache
+        else:
+            query['use_cache'] = None
 
         downloadqueue = DownloadQueue(
             num_threads=self._config['parallel_jobs'],
@@ -94,50 +122,63 @@ class Session:
         return downloadqueue
 
     def _add_to_cache(self, response):
+        """ Write a response tuple (url, data) to local cache. """
         for url, data in response:
             self._cache.write(url, data)
 
+    def _submit(self, query):
+        """ Here be dragons. """
+        results = []
+        downloadqueue = self._init_download_queue(query)
+
+        for job in self._create_jobs_according_to_search_params(query):
+            if job['is_done']:
+                results.append(self._job_to_result(job, query))
+            else:
+                downloadqueue.push(job)
+
+        while True:
+            try:
+                job = downloadqueue.pop()
+            except queue.Empty:
+                break
+
+            response = copy.deepcopy(job['response'])
+
+            # trigger provider to parse its request and process the result
+            job['result'], job['is_done'] = job['provider'].parse_response(
+                job['response'], query
+            )
+
+            if job['result'] and job['result'] != []:
+                self._add_to_cache(response)
+
+            if job['is_done']:
+                self._process_flagged_as_done(
+                    job, downloadqueue, query, results
+                )
+            else:
+                self._process_flagged_as_not_done(
+                    job, downloadqueue, query, results
+                )
+        downloadqueue.push(None)
+        return self._select_results_by_strategy(results, query)
+
     def submit(self, query):
+        """
+        Submit a synchronous search query that blocks until finished.
+
+        :param query: Query object with search parameters.
+        :returns: A list with result objects.
+
+        """
         if self._shutdown_session:
             self.clean_up()
         else:
-            results = []
-            downloadqueue = self._init_download_queue(query)
-
-            for job in self._create_jobs_according_to_search_params(query):
-                if job['is_done']:
-                    results.append(self._job_to_result(job, query))
-                else:
-                    downloadqueue.push(job)
-
-            while True:
-                try:
-                    job = downloadqueue.pop()
-                except queue.Empty:
-                    break
-
-                response = copy.deepcopy(job['response'])
-
-                # trigger provider to parse its request and process the result
-                job['result'], job['is_done'] = job['provider'].parse_response(
-                    job['response'], query
-                )
-
-                if job['result'] and job['result'] != []:
-                    self._add_to_cache(response)
-
-                if job['is_done']:
-                    self._process_flagged_as_done(
-                        job, downloadqueue, query, results
-                    )
-                else:
-                    self._process_flagged_as_not_done(
-                        job, downloadqueue, query, results
-                    )
-            downloadqueue.push(None)
-            return self._select_results_by_strategy(results, query)
+            return self._submit(query)
 
     def submit_async(self, query):
+        """ Invoke :func:`submit` asynchronous. """
         future = self._async_executor.submit(
             self.submit,
             query
@@ -146,12 +187,16 @@ class Session:
         return future
 
     def _process_flagged_as_done(self, job, downloadqueue, query, results):
+        """ Process jobs which are marked as done by provider. """
         if job['is_done'] or job['result'] == []:
             results.append(self._job_to_result(job, query))
 
     def _process_flagged_as_not_done(self, job, downloadqueue, query, results):
+        """ Process jobs which are marked as not done by provider. """
         if job['result']:
-            new_jobs = self._create_new_jobs_from_result(job, query)
+            new_jobs = self._create_new_jobs_from_urls(
+                job['result'], job['provider'], query
+            )
             for job in new_jobs:
                 downloadqueue.push(job)
         else:
@@ -162,17 +207,26 @@ class Session:
                 downloadqueue.push(job)
 
     def _select_results_by_strategy(self, results, query):
+        """
+        Filter result objects according to user specified search strategy.
+
+        :param results: A list with finished results.
+        :param query: The query that belongs to the results given.
+
+        """
         if query['strategy'] == 'deep':
             return self._results_deep_strategy(results, query)
         else:
             return self._results_flat_strategy(results, query)
 
     def _results_deep_strategy(self, results, query):
+        """ Return results proccessed with deep strategy. """
         results.sort(key=lambda x: x.provider._priority, reverse=True)
         results = self._sort_by_ratio(results, query)
         return results[:query['items']]
 
     def _results_flat_strategy(self, results, query):
+        """ Return results proccessed with flat strategy. """
         result_map = defaultdict(list)
         for result in results:
             result_map[result.provider].append(result)
@@ -186,10 +240,12 @@ class Session:
         return results[:query['items']]
 
     def _sort_by_ratio(self, results, query):
+        """ Sort results by ratio between result and search params. """
         ratio_table = []
+        qry_imdb = query['imdbid']
         for result in filter(lambda res: res._result_dict, results):
             ratio = 0.0
-            if query['imdbid'] and query['imdbid'] == result._result_dict['imdbid']:
+            if qry_imdb and qry_imdb == result._result_dict['imdbid']:
                 ratio = 1.0
             elif query['title'] and result._result_dict['title']:
                 ratio = string_similarity_ratio(
@@ -203,6 +259,7 @@ class Session:
         return [res['result'] for res in ratio_table]
 
     def _job_to_result(self, job, query):
+        """ Return a result generated from finished job and query. """
         retries = query['retries'] - job['retries_left']
         result = Result(
             provider=job['provider'],
@@ -213,6 +270,7 @@ class Session:
         return result
 
     def _decrement_retries(self, job):
+        """ Decrement retries inside job, set to done if no retries left. """
         if job['retries_left'] > 0:
             job['retries_left'] -= 1
         else:
@@ -220,18 +278,17 @@ class Session:
         return job
 
     def _get_job_struct(self, provider, query):
-        return {
-            'url': None,
-            'provider': provider,
-            'future': None,
-            'response': None,
-            'is_done': False,
-            'result': None,
-            'return_code': None,
-            'retries_left': query.get('retries')
-        }
+        """ Return a job structure. """
+        params = [
+            'url', 'future', 'response', 'is_done', 'result', 'return_code',
+            'retries_left', 'provider'
+        ]
+        job = {param: None for param in params}
+        job['provider'], job['retries_left'] = provider, query['retries']
+        return job
 
     def _get_provider_for_current_job(self, query):
+        """ Return provider list with according to params in query. """
         providers = []
         for key, value in self._provider_types.items():
             if query['type'] in key:
@@ -239,11 +296,12 @@ class Session:
 
         if query['providers']:
             allowed_provider = [x.upper() for x in query['providers']]
-            prov_filter = lambda x : x['name'].name.upper() in allowed_provider
+            prov_filter = lambda x: x['name'].name.upper() in allowed_provider
             providers = [x for x in filter(prov_filter, providers)]
         return providers
 
     def _create_jobs_according_to_search_params(self, query):
+        """ Create new jobs, according to given search params in query. """
         job_list = []
         for provider in self._get_provider_for_current_job(query):
             provider = provider['name']
@@ -258,39 +316,39 @@ class Session:
 
         return job_list
 
-    def _create_new_jobs_from_result(self, job, query):
-        urllist = []
-        for url_list in job['result']:
-            job = self._get_job_struct(provider=job['provider'], query=query)
+    def _create_new_jobs_from_urls(self, urls, provider, query):
+        """ Create new jobs from urls and a specific provider. """
+        jobs = []
+        for url_list in urls:
+            job = self._get_job_struct(provider=provider, query=query)
             job['url'] = url_list
-            urllist.append(job)
-        return urllist
+            jobs.append(job)
+        return jobs
 
     def providers_list(self, category=None):
+        """ List available providers. """
         if category and category in self._provider_types:
             return self._provider_types[category]
         else:
             return self._provider_types
 
     def provider_types(self):
+        """ List available provider types. """
         return self._provider_types.keys()
 
     def _categorize(self, provider):
+        """ Cagegorizes providers according to its type. """
         self._provider_types[provider.identify_type()].append(
             {'name': provider, 'supported_attrs': provider.supported_attrs}
         )
 
-    def get_providers(self):
-        return self._provider_types
-
     def get_postprocessing(self):
+        """ Return prostprocessing filters, this is for test purposes only. """
         return self._postprocessing
 
     def converter_list(self):
+        """ Return converters, this is for test purposes only. """
         return self._converter
-
-    def config_list(self):
-        return self._config
 
     def clean_up(self):
         """ Do a clean up on keyboard interrupt or submit cancel. """
@@ -313,5 +371,6 @@ class Session:
         """ Cancel the currently running session. """
         self._shutdown_session = True
 
-def signal_handler(self, signal, frame):
-    self.cancel()
+    def signal_handler(self, signal, frame):
+        """ Invoke cancel on signal interrupt. """
+        self.cancel()
