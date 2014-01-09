b53da981e1fe7233997f2f8504348021433c779b
qitta <christop@nullcat.de>
2014-01-09 17:18:28 +0100

    analyzer/modifier templates added.

---
+++ hugin/analyze/plugin_templates/myanalyzer/myanalyzer.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+# hugin
+import hugin.analyze as plugin
+
+
+class MyAnalyzer(plugin.IAnalyzer):
+
+    def __init__(self):
+        pass
+
+    def analyze(self, movie):
+        pass
+
---
+++ hugin/analyze/plugin_templates/mymodifier/mymodifier.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+# hugin
+import hugin.analyze as plugin
+
+
+class MyModifier(plugin.IModifier):
+
+    def __init__(self):
+        pass
+
+    def modify(self, movie):
+        pass
+
