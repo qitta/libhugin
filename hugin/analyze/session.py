3b9ec08229e00e07e58d88f81aa106ee38e467b3
qitta <christop@nullcat.de>
2014-01-09 17:16:16 +0100

    session prototype added.

--- hugin/analyze/session.py
+++ hugin/analyze/session.py
@@ -2,52 +2,40 @@
 # encoding: utf-8
 
 import os
-import sys
-import os
-import glob
-import shelve
-import xmltodict
-from guess_language import guess_language
-from collections import Counter
+import pickle
+from hugin.analyze.movie import Movie
+from hugin.analyze.pluginhandler import PluginHandler
 
-#nfo read helper
-def read_attrs(nfofile, mask):
-    try:
-        with open(nfofile, 'r') as f:
-            xml = xmltodict.parse(f.read())
-            attributes = {key: None for key in mask.keys()}
-            for key, filekey in mask.items():
-                attributes[key] = xml['movie'][filekey]
-            return attributes
-    except Exception as e:
-        print('Exception', e)
-
-MASK = {
-    'title': 'title',
-    'originaltitle': 'originaltitle',
-    'year': 'year',
-    'plot': 'plot',
-    'director': 'director',
-    'genre': 'genre'
-}
 
 class Session:
 
-    def __init__(self, database, attrs=MASK):
+    def __init__(self, database, attr_mask=None):
         self._dbname = database
-        self._database = shelve.open(self._dbname)
-        self._mask = attrs
+        self._database = self.database_open(database)
+        self._mask = attr_mask
 
-    def add(self, nfofile):
+        self._plugin_handler = PluginHandler()
+        self._plugin_handler.activate_plugins_by_category('Analyzer')
+        self._plugin_handler.activate_plugins_by_category('Modifier')
+
+        self._modifier = self._plugin_handler.get_plugins_from_category(
+            'Modifier'
+        )
+        self._analyzer = self._plugin_handler.get_plugins_from_category(
+            'Analyzer'
+        )
+
+    def add(self, nfofile, helper):
+        attrs_mask = {key: None for key in self._mask.keys()}
         if os.path.isdir(nfofile):
             # there is no nfofile, so we get the directory
-            movie = Movie(nfofile, None, None)
+            movie = Movie(nfofile, None, attrs_mask)
         else:
             # we have a nfofile, so we can get the directory
-            attrs = read_attrs(nfofile, self._mask)
-            if attrs is None:
-                attrs = {key: None for key in self._mask.keys()}
-            movie = Movie(os.path.dirname(nfofile), nfofile, attrs)
+            helper_attrs = helper(nfofile, self._mask)
+            if helper_attrs is None:
+                helper_attrs = attrs_mask
+            movie = Movie(os.path.dirname(nfofile), nfofile, helper_attrs)
         self._database[movie.key] = movie
 
     def stats(self):
@@ -55,35 +43,30 @@ class Session:
             self._dbname, len(self._database.keys())
         )
 
-    def database_shutdown(self):
-        self._database.close()
+    def get_database(self):
+        return self._database
 
+    def analyzer_plugins(self, pluginname=None):
+        return self._get_plugin(self._analyzer, pluginname)
 
-class Movie:
+    def modifier_plugins(self, pluginname=None):
+        return self._get_plugin(self._modifier, pluginname)
 
-    def __init__(self, key, nfo, attributes):
-        self.key = key
-        self.nfo = nfo
-        self._attributes = attributes
-        self._analyzer = {}
-
-if __name__ == '__main__':
-    s = Session('movie.db')
-    path = sys.argv[1]
-    c = Counter()
-    for moviefolder in os.listdir(path):
-        full_movie_path = os.path.join(path, moviefolder)
-        nfofile = glob.glob1(full_movie_path, '*.nfo')
-        if nfofile == []:
-            nfofile = full_movie_path
-            c['no_nfo'] += 1
+    def _get_plugin(self, plugins, pluginname=None):
+        if pluginname is None:
+            return plugins
         else:
-            nfofile = os.path.join(full_movie_path, nfofile.pop())
-        s.add(nfofile)
+            for plugin in plugins:
+                if pluginname.upper() in plugin.name.upper():
+                    return plugin
 
-    import pprint
-    for item in dict(s._database).values():
-        if item._attributes and item._attributes.get('plot'):
-            c[guess_language(item._attributes['plot'])] +=1
-    print(s.stats(), c)
-    s.database_shutdown()
+    def database_open(self, database):
+        try:
+            with open(self._dbname, 'rb') as f:
+                return pickle.load(f)
+        except FileNotFoundError as e:
+            return dict()
+
+    def database_shutdown(self):
+        with open(self._dbname, 'wb') as f:
+            pickle.dump(self._database, f)
