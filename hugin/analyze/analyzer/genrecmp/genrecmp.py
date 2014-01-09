101589d3fae6f66a2acf20dc6ac508d383c34781
kitteh <christoph@nullcat.de>
2014-01-09 22:28:45 +0100

    genrecmp updated.

--- hugin/analyze/analyzer/genrecmp/genrecmp.py
+++ hugin/analyze/analyzer/genrecmp/genrecmp.py
@@ -12,10 +12,16 @@ class GenreCmp(plugin.IAnalyzer):
 
     def process_database(self, database):
         genre_distances = []
+        tmp = None
         for a, b in combinations(database.values(), 2):
+            if tmp is None:
+                tmp = a
             genre_distances.append((b, self._genre_distance(a, b)))
+            if tmp is not a:
+                tmp.analyzer_data[self.name] = genre_distances
+                tmp = a
 
 
     def _genre_distance(self, a, b):
         if a.attributes.get('genre') and b.attributes.get('genre'):
-            return  len(set(a.attributes.get('genre')) | set(b.attributes.get('genre'))) / len(set(a.attributes.get('genre')))
+            return  len(set(a.attributes.get('genre')) & set(b.attributes.get('genre'))) / len(set(a.attributes.get('genre')))
