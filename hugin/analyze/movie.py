75ced907f358c47dc4bfe7ef124e4eb6b17da10d
qitta <christop@nullcat.de>
2014-01-09 17:19:59 +0100

    movie representation object added.

---
+++ hugin/analyze/movie.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+class Movie:
+
+    def __init__(self, key, nfo, attributes):
+        self.key = key
+        self.nfo = nfo
+        self.attributes = attributes
+        self.analyzer_data = {}
