6062b04c60de570be9bb0dca9721fa6be7747562
kitteh <christoph@nullcat.de>
2014-02-02 14:44:21 +0100

    String compare invalid title fix.

--- hugin/utils/stringcompare.py
+++ hugin/utils/stringcompare.py
@@ -19,14 +19,16 @@ def string_similarity_ratio(s1, s2):
     """
     s1 = _clean_movie_title(s1)
     s2 = _clean_movie_title(s2)
-    return difflib.SequenceMatcher(None, s1.upper(), s2.upper()).ratio()
+    if s1 and s2:
+        return difflib.SequenceMatcher(None, s1.upper(), s2.upper()).ratio()
 
 
 def _clean_movie_title(title):
-    word_list = title.replace(',', ' ').split()
-    word_list.sort()
-    clean_word_list = [w.strip() for w in word_list]
-    return ' '.join(clean_word_list)
+    if title:
+        word_list = title.replace(',', ' ').split()
+        word_list.sort()
+        clean_word_list = [w.strip() for w in word_list]
+        return ' '.join(clean_word_list)
 
 
 if __name__ == '__main__':
