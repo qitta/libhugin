dbd3b452f53c46424b3dc5d0ae21d65fe8a99531
kitteh <christoph@nullcat.de>
2014-02-04 12:31:46 +0100

    Videobuster update and cleanup for better maintenance.

--- hugin/harvest/provider/videobuster/videobustermovie.py
+++ hugin/harvest/provider/videobuster/videobustermovie.py
@@ -27,7 +27,6 @@ class VIDEOBUSTERMovie(provider.IMovieProvider):
         }
 
     def build_url(self, search_params):
-        print(search_params)
         if search_params.title:
             return [self._base_url.format(
                 quote_plus(search_params.title, encoding='latin-1')
@@ -41,14 +40,12 @@ class VIDEOBUSTERMovie(provider.IMovieProvider):
             print('Unable to parse response.', e)
             return None, True
 
-
         if 'titlesearch.php' in url:
             result = self._parse_search_module(response, search_params)
             if result:
                 return result, False
 
         if 'titledtl.php' in url:
-            print(url)
             result = self._parse_movie_module(response, search_params)
             if result:
                 return result, True
@@ -58,7 +55,7 @@ class VIDEOBUSTERMovie(provider.IMovieProvider):
     def _parse_search_module(self, result, search_params):
         similarity_map = []
         try:
-            for item in result.find_all("div", {"class":"name"}):
+            for item in result.find_all("div", {"class": "name"}):
                 if item.get_text() and item.find("a").get("href"):
                     title = item.get_text()
                     url, _ = item.find("a").get("href").split('?', maxsplit=1)
@@ -82,85 +79,92 @@ class VIDEOBUSTERMovie(provider.IMovieProvider):
     def _parse_movie_module(self, result, search_params):
         result_dict = {k: None for k in self._attrs}
 
-        result_dict['title'] = self._strip_attr(
-            result.find("h1", {"class": "name"}).get_text()
+        result_dict['title'] = self._parse_title(result)
+        result_dict['original_title'] = self._parse_attribute(
+            result, 'Originaltitel'
         )
-        result_dict['original_title'] = self._strip_attr(
-            self._strip_attr(self._extract_item(result, 'Originaltitel'))
+        result_dict['plot'] = self._parse_plot(result)
+
+        countries, year = self._parse_countries_year(result)
+        result_dict['countries'], result_dict['year'] = countries, year
+
+        result_dict['actors'] = self._parse_actors(result)
+        result_dict['poster'] = self._parse_poster(result)
+
+        result_dict['directors'] = self._parse_attribute(result, 'regie')
+        result_dict['genre'] = self._parse_attribute(result, 'genre')
+        result_dict['keywords'] = self._parse_attribute(result, 'Schlagwört')
+        result_dict['studios'] = self._parse_attribute(result, 'Studio')
+
+        return result_dict
+
+    def _parse_title(self, response):
+        title = self._strip_attr(
+            response.find("h1", {"class": "name"}).get_text()
         )
+        return self._strip_attr(title)
 
-        result_dict['plot'], *_ = self._strip_attr(result.find(
-            "div", {"class": "txt movie_description"}).get_text()
-        ).rsplit('\r', maxsplit=1)
-        result_dict['plot'] = str(result_dict['plot'])
+    def _parse_plot(self, response):
+        plot = response.find(
+            "div", {"class": "txt movie_description"}
+        ).get_text()
 
+        # last paragraph of plot dosen't belong to the paragraph, so we want to
+        # split it and throw it away
+        plot, *_ = plot.rsplit('\r', maxsplit=1)
+        return self._strip_attr(plot)
 
-        result_dict['genre'] = self._strip_attr(
-            self._extract_item(result, 'genre').split(',')
-        )
+    def _parse_countries_year(self, response):
+        country_year_tag = self._extract_tag(response, 'produktion')
+        if country_year_tag:
+            countries, year = country_year_tag.rsplit(maxsplit=1)
+            if year.isnumeric():
+                return self._strip_attr(countries.split(',')), int(year)
+            return self._strip_attr(countries.split(',')), None
 
-        # parse director
-        result_dict['directors'] = self._strip_attr(
-            self._extract_item(result, 'regie').split(',')
-        )
-
-        # parse country, year
-        country, year = self._strip_attr(
-            self._extract_item(result, 'produktion').rsplit(maxsplit=1)
-        )
-        result_dict['year'], result_dict['countries'] = int(year), country.split(',')
-
-        # parse actors
-        actors = self._strip_attr(
-            self._extract_item(result, 'Darsteller')
-        )
+    def _parse_actors(self, response):
+        actors = self._extract_tag(response, 'Darsteller')
         if actors:
             actors = [(None, actor) for actor in self._strip_attr(
                 actors.split(',')
             )]
-        result_dict['actors'] = actors
+            return actors
 
-        # get cover, ugly, I know
-        poster, _ = result.find(
+    def _parse_poster(self, response):
+        postertag = response.find(
             "div", {"class": "title_cover_image_box"}
-        ).find("a").get("href").split('?', 1)
-        if poster:
-            result_dict['poster'] = [(None, poster)]
+        ).find("a").get("href")
+        if postertag:
+            poster = postertag.split('?', 1)
+            return [(None, poster)]
 
-        # parse keywords
-        keywords = self._strip_attr(
-            self._extract_item(result, 'Schlagwörter')
-        )
-        if keywords:
-            result_dict['keywords'] = self._strip_attr(keywords.split(','))
+    def _parse_attribute(self, response, attr):
+        attr_list = self._extract_tag(response, attr)
+        if attr_list:
+            if 'Original' in attr:
+                return self._strip_attr(attr_list)
+            else:
+                return self._strip_attr(attr_list.split(','))
 
-        studios = self._strip_attr(
-            self._extract_item(result, 'Studio')
-        )
-        result_dict['studios'] = self._strip_attr(studios.split(','))
-
-        return result_dict
-
-    def _extract_item(self, response, tag):
+    def _extract_tag(self, response, tag):
         content = response.find(
-            "div",{"class":"title_dtl_below_tab active_tab_details"}
+            "div", {"class": "title_dtl_below_tab active_tab_details"}
         )
-        for item in content.find_all("div", {"class":"txt"}):
+        for item in content.find_all("div", {"class": "txt"}):
             try:
-                label = item.find("div", {"class":"label"})
-                content = item.find("div", {"class":"content"})
+                label = item.find("div", {"class": "label"})
+                content = item.find("div", {"class": "content"})
                 if label and content:
-                    if tag.upper().strip() in label.get_text().upper().strip() :
+                    if tag.upper().strip() in label.get_text().upper().strip():
                         return content.get_text()
             except AttributeError as e:
-                print(e)
+                print('AttributeError in _extract_tag videobuster:', e)
 
     def _strip_attr(self, attr):
         if isinstance(attr, list):
             return [s.strip() for s in attr]
         if isinstance(attr, str):
             return attr.strip()
-
 
     @property
     def supported_attrs(self):
