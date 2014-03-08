8c939e532428f11d2418e6d0c52271437a67d1c3
kitteh <christoph@nullcat.de>
2014-03-08 11:58:23 +0100

    nfo converter update

--- hugin/harvest/converter/nfo/nfo.py
+++ hugin/harvest/converter/nfo/nfo.py
@@ -4,18 +4,8 @@
 # hugin
 import hugin.harvest.provider as provider
 
-# http://wiki.xbmc.org/index.php?title=NFO_files/movies
-# key == nfo attr, item == result_dict key for the specific attr
-nfo_dict_map = {
-    'title': 'title', 'originaltitle': 'original_title', 'sorttitle': 'title',
-    'set': 'collection', 'rating': 'rating', 'year': 'year', 'top250':
-    'top250', 'votes': 'vote_count', 'outline': 'outline', 'plot': 'plot',
-    'tagline': 'tagline', 'runtime': 'runtime', 'thumb': 'poster', 'mppa':
-    'mppa', 'playcount': 'playcount', 'id': 'imdbid', 'filename': 'filename',
-    'trailer': 'trailers', 'country': 'countries', 'genre': 'genre', 'credits':
-    'writers', 'FILEINFO': 'FILEINFO', 'director': 'directors', 'ACTORS':
-    'ACTORS'
-}
+# Simple xbmc nfo converter plugin
+# nfo specs taken from http://wiki.xbmc.org/index.php?title=NFO_files/movies
 
 
 class Nfo(provider.IOutputConverter):
@@ -34,35 +24,49 @@ class Nfo(provider.IOutputConverter):
 
     def convert(self, result):
         if result and result._result_type == 'movie':
-            return self._create_nfo(result)
+            return self._create_nfo(result._result_dict)
 
 # Helper functions
     def _create_nfo(self, result):
+        nfo_dict_map = self._get_nfo_result_dict()
         for nfo_tag, dict_key in nfo_dict_map.items():
             self._set_value(nfo_dict_map, nfo_tag, dict_key, result)
         return self._base.format(**nfo_dict_map)
 
     def _set_value(self, nfo_dict_map, nfo_tag, dict_key, result):
 
-        if dict_key in result._result_dict:
+        close_tag = '{close_tag}'.format(close_tag=dict_key)
+        if dict_key in result:
 
             if dict_key in ['genre', 'directors', 'writers', 'countries']:
                 nfo_dict_map[nfo_tag] = self._create_multi_tag(
-                    result._result_dict[dict_key], nfo_tag
+                    result[dict_key], nfo_tag
                 )
             elif dict_key in ['trailers', 'poster']:
                 nfo_dict_map[nfo_tag] = self._extract_tuple_attr(
-                    result._result_dict[dict_key]
+                    result[dict_key], dict_key
                 )
             else:
-                nfo_dict_map[nfo_tag] = result._result_dict[dict_key] or ''
+                nfo_dict_map[nfo_tag] = result[dict_key] or close_tag
         else:
             if dict_key in ['ACTORS']:
                 nfo_dict_map[nfo_tag] = self._create_actor_section(
-                    result._result_dict['actors']
+                    result['actors']
                 )
             else:
-                nfo_dict_map[nfo_tag] = ''
+                nfo_dict_map[nfo_tag] = close_tag
+
+    def _get_nfo_result_dict(self):
+        return {
+            'title': 'title', 'originaltitle': 'original_title', 'sorttitle':
+            'title', 'set': 'collection', 'rating': 'rating', 'year': 'year',
+            'top250': 'top250', 'votes': 'vote_count', 'outline': 'outline',
+            'plot': 'plot', 'tagline': 'tagline', 'runtime': 'runtime',
+            'thumb': 'poster', 'mppa': 'mppa', 'playcount': 'playcount', 'id':
+            'imdbid', 'filename': 'filename', 'trailer': 'trailers', 'country':
+            'countries', 'genre': 'genre', 'credits': 'writers', 'FILEINFO':
+            'FILEINFO', 'director': 'directors', 'ACTORS': 'ACTORS'
+        }
 
     def _create_multi_tag(self, attr, tagname):
         tag = '<{tagname}>{{attr}}</{tagname}>\n'.format(tagname=tagname)
@@ -79,22 +83,14 @@ class Nfo(provider.IOutputConverter):
                 actors.append(self._actors.format(name=name, role=role))
             return ''.join(actors)
 
-    def _extract_tuple_attr(self, result):
+    def _extract_tuple_attr(self, result, dict_key):
         if result:
-            for _, item in result:
-                return item
+            for size, item in result:
+                if dict_key == 'poster' and size == 'original':
+                    return item
+                elif dict_key == 'trailers':
+                    return item
 
     def _open_template(self, template):
         with open(template, 'r') as f:
             return f.read()
-
-if __name__ == '__main__':
-    from hugin.harvest.session import Session
-    s = Session()
-    q = s.create_query(title='Sin', amount=5)
-    r = s.submit(q)
-    p = Nfo()
-    print(r[0])
-    print(p.convert(r[0]))
-    print('FILM:', r[0])
-    print(p.convert(r[0]))
