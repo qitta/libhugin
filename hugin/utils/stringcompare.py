8757b343286a619da2c8d21b07435de95b7f81ee
kitteh <christoph@nullcat.de>
2014-03-20 15:21:14 +0100

    movie clean title case sensetive sort bug fixed.

--- hugin/utils/stringcompare.py
+++ hugin/utils/stringcompare.py
@@ -20,11 +20,12 @@ def string_similarity_ratio(s1, s2):
     s1 = _clean_movie_title(s1)
     s2 = _clean_movie_title(s2)
     if s1 and s2:
-        return difflib.SequenceMatcher(None, s1.upper(), s2.upper()).ratio()
+        return difflib.SequenceMatcher(None, s1, s2).ratio()
 
 
 def _clean_movie_title(title):
     if title:
+        title = title.upper()
         word_list = title.replace(',', ' ').split()
         word_list.sort()
         clean_word_list = [w.strip() for w in word_list]
